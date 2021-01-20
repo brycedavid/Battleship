32e919f7d41b012d2b8693c19546fe28d2a0c828
Bryce David Landry <brycedavid66@gmail.com>
2021-01-20 16:37:33 -0600

    Update ToPerpetrator.java
    Add comments

--- Battleship_Server/src/Battleship/ToPerpetrator.java
+++ Battleship_Server/src/Battleship/ToPerpetrator.java
@@ -3,16 +3,22 @@ package Battleship;
 import java.io.*;
 import java.net.*;
 
+//*********************************************************************************************************************
+//Author: Bryce Landry
+//This class Forwards information to the perpetrator from the victim. Created/called by FromVictim class.
+//*********************************************************************************************************************
 public class ToPerpetrator{
 
     Socket sock;
     DataOutputStream out = null;
 
+    //constructor
     public ToPerpetrator(Socket sock)  throws Exception {
         this.sock = sock;
         out = new DataOutputStream(sock.getOutputStream());
     }
 
+    //Forward message from victim to the perpetrator.
     public void write(String message){
         try{
             out.writeBytes(message + "\n");
