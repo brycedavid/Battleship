c5506f8ec5fa4fc4c15443cebcb3e5497623a122
Bryce David Landry <brycedavid66@gmail.com>
2021-01-24 13:27:45 -0600

    Update Main.java
    More meaningful comments

--- Battleship/src/Battleship/Main.java
+++ Battleship/src/Battleship/Main.java
@@ -1,9 +1,3 @@
-// Bryce Landry
-// Finish Date : 3/19/2020
-// This program acts as a client, which connects to the server (on port 8088) that hosts another client. Once
-// two clients have connected, a game of salvo starts between them.
-//**For Server details, see server program**//
-
 package Battleship;
 
 import javafx.application.Application;
@@ -12,24 +6,36 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+//*********************************************************************************************************************
+//Author: Bryce Landry
+//This program acts as a client to the server program, titled "Battleship_Server". This program connects to the server
+//using sockets on port 8088. The game is to be played either on one laptop or from two laptops on the same network,
+//as per usual with using ports. Once the server program is up and running, two copies of this program can be run and
+//will connect to the server (server hosts 2 of these at a time). Upon both connecting, the game will start.
+//*********************************************************************************************************************
 
-//This class sets up the UI and launches the game for the client that runs it. It initializes the Controller class.
+
+//This class sets up the UI and launches the game for the client that runs it. It also initializes the Controller class.
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
+        //Load sample.fxml
         FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+
+        //Initialize Controller class
         loader.setController(new Controller());
         Parent root = loader.load();
 
-        primaryStage.setTitle("Salvo!");
+        //Set the stage
+        primaryStage.setTitle("Battleship!");
         primaryStage.setScene(new Scene(root, 750, 700));
         primaryStage.show();
 
     }
 
-
+    //Start the game once UI is loaded
     public static void main(String[] args) {
         launch(args);
     }
